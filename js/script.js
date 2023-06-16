c21f201dacddc61eeaf6932c1038216ac806b326
kulae8 <kulae8@mail.ru>
2023-06-16 15:05:52 +0300

    slider 1

---
+++ js/script.js
@@ -0,0 +1,20 @@
+const a = 1;
+const b = 2; 
+const result = a + b;
+
+switch (result) {
+    case 0:
+        console.log('результат равен 0');
+        break;
+    case 1:
+        console.log('результат равен 1');
+        break;
+    case 2:
+        console.log('результат равен 2');
+        break;
+ case 3:
+        console.log('результат равен 3');
+        break;
+    default:
+        console.log('результат не равен числам 1,2,3');
+}
