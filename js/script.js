89be18ce23d7d8944a478dd32852934a986fae98
kulae8 <kulae8@mail.ru>
2023-06-23 14:42:41 +0300

    23.06

--- js/script.js
+++ js/script.js
@@ -1,24 +1,39 @@
 const slider = [
     {
       sliderCity: 'Rostov-on-Don\nLCD admiral',
+      sliderArea: '81 m2',
+      sliderTime: '3,5 months',
       img: './img/home-top.jpg'
     },
     {
       sliderCity: 'Sochi\nThieves',
+      sliderArea: '105 m2',
+      sliderTime: '4 months',
       img: './img/home-top2.jpg'
     },
     {
       sliderCity: 'Rostov-on-Don\nPatriotic',
+      sliderArea: '93 m2',
+      sliderTime: '3 months',
       img: './img/home-top3.jpg'
     }
   ]
   
   const sliderCity = document.querySelector('.sliderCity')
+  const sliderArea = document.querySelector('.sliderArea')
+  const sliderTime = document.querySelector('.sliderTime')
   const img = document.querySelector('.image')
   
   const setImage = (index) => {
     sliderCity.innerText = slider[index].sliderCity
     img.style.backgroundImage = `url(${slider[index].img})`
+    {
+      sliderArea.innerText = slider[index].sliderArea
+    img.style.backgroundImage = `url(${slider[index].img})`
+    {
+      sliderTime.innerText = slider[index].sliderTime
+    img.style.backgroundImage = `url(${slider[index].img})`
+    }}
   }
   
   const prev = document.querySelector('.prev')
